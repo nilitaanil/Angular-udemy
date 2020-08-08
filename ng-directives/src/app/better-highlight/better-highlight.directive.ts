cdc13cbb32e396c0e6a079feaa54c9d6b851db3a
nilitaanil <nilita.aniill@gmail.com>
2020-08-08 12:18:49 -0400

    ng-directives

---
+++ ng-directives/src/app/better-highlight/better-highlight.directive.ts
@@ -0,0 +1,30 @@
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor: string = 'transparent';
+  @Input() highlightColor: string = 'blue';
+  // @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+  @HostBinding('style.backgroundColor') backgroundColor: string ;
+
+  constructor(private elRef: ElementRef,private renderer: Renderer2) { }
+
+  ngOnInit(){
+    // this.renderer.setStyle(this.elRef.nativeElement, "background-color", 'blue', false, false);
+    this.backgroundColor = this.defaultColor;
+  }
+
+  @HostListener('mouseenter') mouseover(eventdata: Event){
+    // this.renderer.setStyle(this.elRef.nativeElement, "background-color", 'blue');
+    // this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
+  }
+  @HostListener('mouseleave') mouseleave(eventdata: Event){
+    // this.renderer.setStyle(this.elRef.nativeElement, "background-color", 'transparent');
+    // this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
+  }
+
+}
